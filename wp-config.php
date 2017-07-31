11d4c0ca2bc92b790919b1a8756fd16958ebbfcc
thanetwerk <30486671+thanetwerk@users.noreply.github.com>
2017-07-31 11:15:42 -0400

    localhost

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', ‘root’);
 define('DB_PASSWORD', ‘root’);
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost:8889’);
+define('DB_HOST', 'localhost’);
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', '');
