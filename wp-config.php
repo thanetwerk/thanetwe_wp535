ddc8f69bc709c6dda75fb3946b9d672876760f1b
thanetwerk <30486671+thanetwerk@users.noreply.github.com>
2017-08-16 23:46:51 -0400

    hope it works

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', ‘thanetwe_wp535’);
+define('DB_NAME', ‘thanetwe.me’);
 
 /** MySQL database username */
 define('DB_USER', ‘thanetwe_wp535’);
